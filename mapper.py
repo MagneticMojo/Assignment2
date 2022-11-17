ec37a0769d83cb614dbca2ff3efa4ad2ad6ead8c
MagneticMojo <forsberg.bjorn.a@gmail.com>
2022-11-17 13:01:14 +0100

    first commit

---
+++ mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+	
+	# remove trailing spaces at end of each line
+	word = line.strip()
+
+	# print mapper output to STOUT (standard output): key <tab> value
+	# TODO [FIX ME!]
+	x = word, 1
+	
+
